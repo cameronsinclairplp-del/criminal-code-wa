54797ba9dd97fab37876d3e0d1b333f9ac947f16
cameronsinclairplp-del <cameron.sinclair.plp@gmail.com>
2026-06-12 22:43:49 +0800

    v10 rebuild — shell + sw cut-over (wal3-e1d462fd17)

--- sw.js
+++ sw.js
@@ -1,60 +1,86 @@
-/* WA Legislation — offline cache (auto-generated) */
-const VERSION = "wal-78f41516ee";
-const ASSETS = ["./", "./index.html", "./manifest.webmanifest",
-  "./icons/icon-180.png", "./icons/icon-192.png", "./icons/icon-512.png", "./icons/icon-maskable-512.png",
-  "./data/cc.json",
-  "./data/cia.json",
-  "./data/ipa.json",
-  "./data/mda.json",
-  "./data/ea.json",
-  "./data/ea25.json",
-  "./data/cpa.json",
-  "./data/roa.json",
-  "./data/sa.json",
-  "./data/rta.json",
-  "./data/fa.json",
-  "./data/voc.json",
-  "./data/cicpa.json",
-  "./data/sda.json",
-  "./data/ba.json",
-  "./data/yoa.json",
-  "./data/ccsa.json",
-  "./data/tia.json",
-  "./data/rtaa.json",
-  "./data/rtva.json",
-  "./data/cpca.json",
-  "./data/wa.json",
-  "./data/wr.json",
-  "./data/docinv.json",
-  "./data/docint.json",
-  "./data/docsus.json",
-  "./data/docca.json",
-  "./data/docsim.json",
-  "./data/docrom.json",
-  "./data/docsda.json",
-  "./data/docswp.json",
-  "./data/docwsr.json",
-  "./data/docstyle.json",
-  "./data/rcl.json",
-  "./data/rcav.json",
-  "./data/rco.json",
-  "./data/rcri.json",
-  "./data/rdc.json",
-  "./data/retc.json",
-  "./data/rem.json",
-  "./data/rhsm.json",
-  "./data/rid.json",
-  "./data/rint.json",
-  "./data/rii.json",
-  "./data/rop.json",
-  "./data/rro.json",
-  "./data/rsw.json",
-  "./data/rst.json",
-  "./data/rsd.json",
-  "./data/rti.json",
-  "./data/ryj.json"];
+/* WA Legislation v3 — offline cache (auto-generated; atomic precache + progress channel) */
+const VERSION = "wal3-e1d462fd17";
+const ASSETS = [
+"./",
+"./index.html",
+"./manifest.webmanifest",
+"./icons/icon-180.png",
+"./icons/icon-192.png",
+"./icons/icon-512.png",
+"./icons/icon-maskable-512.png",
+"./data/ba.json",
+"./data/cc.json",
+"./data/ccsa.json",
+"./data/cia.json",
+"./data/cicpa.json",
+"./data/cpa.json",
+"./data/cpca.json",
+"./data/docca.json",
+"./data/docint.json",
+"./data/docinv.json",
+"./data/docrom.json",
+"./data/docsda.json",
+"./data/docsim.json",
+"./data/docstyle.json",
+"./data/docsus.json",
+"./data/docswp.json",
+"./data/docwsr.json",
+"./data/ea.json",
+"./data/ea25.json",
+"./data/fa.json",
+"./data/ipa.json",
+"./data/mda.json",
+"./data/rcav.json",
+"./data/rcl.json",
+"./data/rco.json",
+"./data/rcri.json",
+"./data/rdc.json",
+"./data/rem.json",
+"./data/retc.json",
+"./data/rhsm.json",
+"./data/rid.json",
+"./data/rii.json",
+"./data/rint.json",
+"./data/roa.json",
+"./data/rop.json",
+"./data/rro.json",
+"./data/rsd.json",
+"./data/rst.json",
+"./data/rsw.json",
+"./data/rta.json",
+"./data/rtaa.json",
+"./data/rti.json",
+"./data/rtva.json",
+"./data/ryj.json",
+"./data/sa.json",
+"./data/sda.json",
+"./data/study.json",
+"./data/tia.json",
+"./data/topics.json",
+"./data/voc.json",
+"./data/wa.json",
+"./data/wr.json",
+"./data/yoa.json"
+];
+const bc = ("BroadcastChannel" in self) ? new BroadcastChannel("wal3-sw") : null;
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(VERSION).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+  e.waitUntil((async () => {
+    const c = await caches.open(VERSION);
+    let done = 0; const total = ASSETS.length; const Q = [...ASSETS];
+    const work = Array.from({ length: 4 }, async () => {
+      while (Q.length){
+        const a = Q.shift(); if (!a) break;
+        const res = await fetch(a, { cache: "no-cache" });
+        if (!res.ok) throw new Error("precache " + a + " -> " + res.status);
+        await c.put(a, res);
+        done++; if (bc) bc.postMessage({ t: "prog", done, total });
+      }
+    });
+    try { await Promise.all(work); }
+    catch (err) { await caches.delete(VERSION); throw err; }   /* atomic: no half-cache ever activates */
+    if (bc) bc.postMessage({ t: "done", total });
+    await self.skipWaiting();
+  })());
 });
 self.addEventListener("activate", e => {
   e.waitUntil(caches.keys().then(keys =>
